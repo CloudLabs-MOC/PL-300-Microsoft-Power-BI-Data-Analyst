d24eda7d4f92c4515db7c96ac316a4110b44fb0e
manishrps <55728764+manishrps@users.noreply.github.com>
2024-03-19 21:55:02 +0530

    Update UpdateDatabase-2-AddSales.ps1

--- Allfiles/Setup/UpdateDatabase-2-AddSales.ps1
+++ Allfiles/Setup/UpdateDatabase-2-AddSales.ps1
@@ -1,6 +1,6 @@
 Write-Host -foreground green 'Inserting June 2020 sales...';
-
+ 
 Invoke-SqlCmd -Server localhost -Database "AdventureWorksDW2020" -InputFile "Scripts\UpdateDatabase-2.sql" -TrustServerCertificate:$true
-
+ 
 Write-Host 'Press any key to close...';
 $z = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown");
