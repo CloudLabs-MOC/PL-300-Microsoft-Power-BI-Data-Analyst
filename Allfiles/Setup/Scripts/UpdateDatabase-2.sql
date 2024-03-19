ffcec687d0395fcc705b853270ef754a887a8f2c
manishrps <55728764+manishrps@users.noreply.github.com>
2024-03-19 21:42:33 +0530

    Update UpdateDatabase-2.sql

--- Allfiles/Setup/Scripts/UpdateDatabase-2.sql
+++ Allfiles/Setup/Scripts/UpdateDatabase-2.sql
@@ -1,12 +1,13 @@
 UPDATE [dbo].[FactResellerSales] SET [ShipDate] = DATEADD(DAY, 7, [OrderDate]) WHERE [ShipDate] IS NULL;
 
 --Add June 2020 sales
-BULK INSERT [dbo].[FactResellerSales] FROM 'D:\PL300\Resources\ResellerSales_202006.csv' WITH
+BULK INSERT [dbo].[FactResellerSales] FROM 'C:\PL300\PL-300-Microsoft-Power-BI-Data-Analyst-prod\Allfiles\Resources\ResellerSales_202006.csv' WITH
 (
 	DATAFILETYPE = 'widechar'
 	,FIRSTROW = 2
 	,FIELDTERMINATOR = ','
 	,ROWTERMINATOR = '\n'
 	,TABLOCK
+	
 );
 GO
